2debf0cf9653599bd0c1ee5051f258ef2fec1129
Grégory Alvim <gregori.jagua@gmail.com>
2023-04-29 11:12:30 -0300

    Feat: created styles to CoffeeItem component

---
+++ src/pages/Home/components/CoffeeItem/index.tsx
@@ -0,0 +1,16 @@
+import { Coffee } from "../../../../data/coffees";
+import { ItemContainer, TagsContainer, TagItem } from "./styles";
+
+export function CoffeeItem({id, title, description, price, tags, image, amount }: Coffee) {
+   return (
+      <ItemContainer>
+         <img src={image} alt="" />
+
+         <TagsContainer>
+            {tags.map(tag => (
+               <TagItem key={tag.id}>{tag.name}</TagItem>
+            ))}
+         </TagsContainer>
+      </ItemContainer>
+   )
+}
