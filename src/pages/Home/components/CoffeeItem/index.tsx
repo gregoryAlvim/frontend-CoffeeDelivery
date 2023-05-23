ac2b177176fc61a649c58b7965cfb45e38d3570b
Grégory Alvim <gregori.jagua@gmail.com>
2023-05-22 22:56:08 -0300

    Feat: added Amount itens container with its itens

--- src/pages/Home/components/CoffeeItem/index.tsx
+++ src/pages/Home/components/CoffeeItem/index.tsx
@@ -1,4 +1,5 @@
 import { Coffee } from "../../../../data/coffees";
+import { Plus, Minus, ShoppingCart } from "phosphor-react";
 import { ItemContainer, TagsContainer, TagItem, PriceCartContainer, AmountItensContainer } from "./styles";
 
 export function CoffeeItem({id, title, description, price, tags, image, amount }: Coffee) {
@@ -20,8 +21,12 @@ export function CoffeeItem({id, title, description, price, tags, image, amount }
             <p>R$ <span>{price.toFixed(2).toString().replace("." , ",")}</span></p>
 
             <AmountItensContainer>
-               
+               <Minus className="minusAndPlus" size={14} weight="bold" />
+               <span>1</span>
+               <Plus className="minusAndPlus" size={14} weight="bold" />
             </AmountItensContainer>
+
+            <ShoppingCart className="shoppingCart" size={38} weight="fill" />
          </PriceCartContainer>
       </ItemContainer>
    )
