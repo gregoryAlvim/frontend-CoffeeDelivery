9a7add3a6ba11bcd45952e6655fa9b25d4395137
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-15 21:03:56 -0300

    Feat: created data to the payment methods

---
+++ src/data/paymentMethods.ts
@@ -0,0 +1,19 @@
+import { CreditCard, Money, Bank } from "phosphor-react";
+
+export const paymentMethods = [
+   {
+     id: 1,
+     description: 'CARTÃO DE CRÉDITO',
+     icon: CreditCard,
+   },
+   {
+     id: 2,
+     description: 'CARTÃO DE DEBITO',
+     icon: Bank,
+   },
+   {
+     id: 3,
+     description: 'DINHEIRO',
+     icon: Money,
+   },
+ ]
