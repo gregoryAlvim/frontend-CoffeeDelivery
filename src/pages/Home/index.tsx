49694aef3acb14db1299c3f75f1c21c46c802dc0
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-17 11:53:32 -0300

    Feat: added the functions of context coffee to change the amount coffee

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,74 +1,74 @@
+import { useTheme } from 'styled-components'
+import { ShoppingCart, Package, Timer, Coffee } from '@phosphor-icons/react'
 
-import { useContext } from 'react';
-import { useTheme } from 'styled-components';
-import { TextWithIcon } from '../../components/TextWithIcon';
-import { ShoppingCart, Package, Timer, Coffee } from '@phosphor-icons/react';
+import { useCoffee } from '../../contexts/CoffeesContext'
+import { TextWithIcon } from '../../components/TextWithIcon'
+import { CoffeeItem } from './components/CoffeeItem'
 
-import { CoffeesContext } from '../../contexts/CoffeesContext';
-import sloganCoffeeDelivery from '../../assets/sloganCoffeeDelivery.svg';
-import { CoffeesContainer, DescriptionSloganContainer, DescriptionSloganItensContainer, HomeContainer, ShowcaseContainer, SloganContainer } from "./styles";
-import { CoffeeItem } from './components/CoffeeItem';
+import sloganCoffeeDelivery from '../../assets/sloganCoffeeDelivery.svg'
+import {
+  CoffeesContainer,
+  DescriptionSloganContainer,
+  DescriptionSloganItensContainer,
+  HomeContainer,
+  ShowcaseContainer,
+  SloganContainer,
+} from './styles'
 
 export function Home() {
-   
-   const theme = useTheme();
-   const { dataCoffees } = useContext(CoffeesContext);
+  const theme = useTheme()
 
-   return (
-      <HomeContainer>
-         <SloganContainer>
-            <DescriptionSloganContainer>
-               <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-               <h2>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h2>
+  const { dataCoffees } = useCoffee()
 
-               <DescriptionSloganItensContainer>
-                  <TextWithIcon 
-                     icon={ShoppingCart} 
-                     color={theme.white} 
-                     backgroundColor={"yellow500"}
-                     description="Compra simples e segura"
-                  />
-                  <TextWithIcon 
-                     icon={Package} 
-                     color={theme.white} 
-                     backgroundColor={"gray600"} 
-                     description="Embalagem mantém o café intacto"
-                  />
-                  <TextWithIcon 
-                     icon={Timer} 
-                     color={theme.white} 
-                     backgroundColor={"yellow300"} 
-                     description="Entrega rápida e rastreada"
-                  />
-                  <TextWithIcon 
-                     icon={Coffee} 
-                     color={theme.white} 
-                     backgroundColor={"purple300"} 
-                     description="O café chega fresquinho até você"
-                  />
-               </DescriptionSloganItensContainer>
-            </DescriptionSloganContainer>
+  return (
+    <HomeContainer>
+      <SloganContainer>
+        <DescriptionSloganContainer>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <h2>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </h2>
 
-            <img src={sloganCoffeeDelivery} alt="" />
-         </SloganContainer>
+          <DescriptionSloganItensContainer>
+            <TextWithIcon
+              icon={ShoppingCart}
+              color={theme.white}
+              backgroundColor={'yellow500'}
+              description="Compra simples e segura"
+            />
+            <TextWithIcon
+              icon={Package}
+              color={theme.white}
+              backgroundColor={'gray600'}
+              description="Embalagem mantém o café intacto"
+            />
+            <TextWithIcon
+              icon={Timer}
+              color={theme.white}
+              backgroundColor={'yellow300'}
+              description="Entrega rápida e rastreada"
+            />
+            <TextWithIcon
+              icon={Coffee}
+              color={theme.white}
+              backgroundColor={'purple300'}
+              description="O café chega fresquinho até você"
+            />
+          </DescriptionSloganItensContainer>
+        </DescriptionSloganContainer>
 
-         <ShowcaseContainer>
-            <h3>Nossos cafés</h3>
-            <CoffeesContainer>
-               {dataCoffees.map((coffee) => (
-                     <CoffeeItem
-                        key={coffee.id}
-                        id={coffee.id}
-                        title={coffee.title}
-                        description={coffee.description}
-                        price={coffee.price}
-                        tags={coffee.tags}
-                        image={coffee.image}
-                        amount={coffee.amount}
-                     />
-               ))} 
-            </CoffeesContainer>
-         </ShowcaseContainer>
-      </ HomeContainer>
-   );
+        <img src={sloganCoffeeDelivery} alt="" />
+      </SloganContainer>
+
+      <ShowcaseContainer>
+        <h3>Nossos cafés</h3>
+        <CoffeesContainer>
+          {dataCoffees.map((coffee) => (
+            <CoffeeItem key={coffee.id} {...coffee} />
+          ))}
+        </CoffeesContainer>
+      </ShowcaseContainer>
+    </HomeContainer>
+  )
 }
