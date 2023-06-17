820771e9b5d406af9409b1f40b087a09dec43533
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-17 11:54:13 -0300

    Feat: created the coffee context

--- src/contexts/CoffeesContext.tsx
+++ src/contexts/CoffeesContext.tsx
@@ -1,24 +1,65 @@
-import { createContext, ReactNode } from "react";
-import { Coffee, coffees } from "../data/coffees";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { Coffee } from '../@types/mockes'
+
+import { coffees } from '../data/coffees'
 
 interface CoffeeContextType {
-   dataCoffees: Coffee[];
+  dataCoffees: Coffee[]
+  handleUpdateCoffeeAmount: (type: 'add' | 'remove', coffeeId: number) => void
 }
 
 interface CoffeesContextProviderProps {
-   children: ReactNode;
- }
-
-export const CoffeesContext = createContext({} as CoffeeContextType);
-
-export function CoffeesContextProvider({ children }: CoffeesContextProviderProps) {
-
-   const dataCoffees = coffees;
-
-   return (
-      <CoffeesContext.Provider value={{dataCoffees}}>
-        {children}
-      </CoffeesContext.Provider>
-    )
+  children: ReactNode
 }
 
+export const CoffeesContext = createContext({} as CoffeeContextType)
+
+export function CoffeesContextProvider({
+  children,
+}: CoffeesContextProviderProps) {
+  const [dataCoffees, setDataCoffees] = useState<Coffee[]>([])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataCoffees(coffees)
+    }, 1000)
+  }, [])
+
+  function handleUpdateCoffeeAmount(type: 'add' | 'remove', coffeeId: number) {
+    const coffeesParsed = dataCoffees?.map((coffee) => {
+      if (coffeeId === coffee.id) {
+        const AddRemoveItemAmount =
+          type === 'add' ? coffee.amount + 1 : coffee.amount - 1
+
+        const updatedItem = {
+          ...coffee,
+          amount: AddRemoveItemAmount,
+          total: coffee.price * AddRemoveItemAmount,
+        }
+
+        return { ...updatedItem }
+      }
+      return coffee
+    })
+
+    return setDataCoffees(coffeesParsed)
+  }
+
+  return (
+    <CoffeesContext.Provider value={{ dataCoffees, handleUpdateCoffeeAmount }}>
+      {children}
+    </CoffeesContext.Provider>
+  )
+}
+
+export function useCoffee() {
+  const context = useContext(CoffeesContext)
+
+  return context
+}
