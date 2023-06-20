cfb864b0a48bf6757277d56d4eb9e76b43cdba9d
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-20 18:46:25 -0300

    Feat: added methods of cart context and created handle functions

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,133 +1,296 @@
-import { useState } from "react";
-import { z } from "zod";
-import { useForm, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { CurrencyDollar, MapPinLine } from "phosphor-react";
+import { useState } from 'react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { CurrencyDollar, MapPinLine, Minus, Plus, Trash } from 'phosphor-react'
 
-import { FormPayment } from "./components/FormPayment";
+import { FormPayment } from './components/FormPayment'
 
-import { paymentMethods } from "../../data/paymentMethods";
-import { CheckoutMain, CheckoutContainer, CheckoutTitle, FormContainer, TextSpan, TextParagraph, InputForm, ResumeCoffeesContainer } from "./styles";
+import { paymentMethods } from '../../data/paymentMethods'
+import {
+  CheckoutMain,
+  CheckoutContainer,
+  CheckoutTitle,
+  FormContainer,
+  TextSpan,
+  TextParagraph,
+  InputForm,
+  CoffeeCard,
+  ResumeCheckoutContainer,
+  AmountItensContainer,
+  CoffeeImage,
+  CoffeeTitle,
+  CoffeePrice,
+  RemoveCoffeeButton,
+  ResumeItemsContainer,
+  CoffeeCardRow,
+  CoffeeRow,
+  ResumeTotalCartContainer,
+  TitleAndPriceComponent,
+  TitleResumeCart,
+  PriceResumeCart,
+  CheckedOrderButton,
+} from './styles'
+import { useCart } from '../../contexts/CartContext'
+import { useNavigate } from 'react-router-dom'
+import { Cart } from '../../@types/mockes'
+import { useTheme } from 'styled-components'
 
 const createOrderFormSchema = z.object({
-   cep: z.string().nonempty('O cep é obrigatório').min(7, 'O cep está incompleto').max(8).transform(cep => cep.replace('-', '')),
-   rua: z.string().nonempty('A rua é obrigatório'),
-   numero: z.string().nonempty('O numero é obrigatório'),
-   complemento: z.string(),
-   bairro: z.string().nonempty('O bairro é obrigatório'),
-   cidade: z.string().nonempty('A cidade é obrigatório'),
-   uf: z.string().nonempty('A UF é obrigatório'),
+  cep: z
+    .string()
+    .nonempty('O cep é obrigatório')
+    .min(7, 'O cep está incompleto')
+    .max(8)
+    .transform((cep) => cep.replace('-', '')),
+  rua: z.string().nonempty('A rua é obrigatório'),
+  numero: z.string().nonempty('O numero é obrigatório'),
+  complemento: z.string(),
+  bairro: z.string().nonempty('O bairro é obrigatório'),
+  cidade: z.string().nonempty('A cidade é obrigatório'),
+  uf: z.string().nonempty('A UF é obrigatório'),
 })
 
-type CreateOrderFormData = z.infer<typeof createOrderFormSchema>
+export type CreateOrderFormData = z.infer<typeof createOrderFormSchema>
 
 export function Checkout() {
+  const [paymentMethodSelected, setPaymentMethodSelected] = useState('')
+  const {
+    items,
+    removeFromCart,
+    updateCoffeeAmountFromCart,
+    clearCart,
+    cartSubTotal,
+    cartTotal,
+    freight,
+  } = useCart()
 
-   const [paymentMethodSelected, setPaymentMethodSelected] = useState('');
+  const navigate = useNavigate()
+  const theme = useTheme()
 
-   const { register, handleSubmit, formState: { errors } } = useForm<CreateOrderFormData>({
-      resolver: zodResolver(createOrderFormSchema)
-   });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateOrderFormData>({
+    resolver: zodResolver(createOrderFormSchema),
+  })
 
-   function handleChangePaymentMethods(selectedPaymentMethod: string) {
-      setPaymentMethodSelected(selectedPaymentMethod)
-   }
+  function handleChangePaymentMethods(selectedPaymentMethod: string) {
+    setPaymentMethodSelected(selectedPaymentMethod)
+  }
 
-   function orderData(addressData: CreateOrderFormData) {
-      console.log({ addressData, paymentMethodSelected });
-   }
+  function handleOrderData(addressData: CreateOrderFormData) {
+    clearCart()
+    console.log(addressData)
+    navigate('/success', { state: { addressData, paymentMethodSelected } })
+  }
 
-   return (
-      <CheckoutMain>
-         <CheckoutContainer className="firstContainer">
-            <CheckoutTitle>Complete seu pedido</CheckoutTitle>
+  function handleUpdateCoffeeAmount(type: 'add' | 'remove', coffeeId: number) {
+    updateCoffeeAmountFromCart(type, coffeeId)
+  }
 
-            <form onSubmit={handleSubmit(orderData)}>
-               <FormContainer className="formAddress">
-                  <CheckoutContainer className="titleFormAndPaymentContainer">
-                     <MapPinLine className="mapPinLine" size={22} />
-                     <TextSpan>Endereço de Entrega</TextSpan>
-                     <TextParagraph>Informe o endereço onde deseja receber seu pedido</TextParagraph>
-                  </CheckoutContainer>
+  function handleItemDelete(data: Cart) {
+    removeFromCart(data)
+  }
 
-                  <div className="formInputsAddressContainer">
-                     <InputForm
-                        gridArea="cep"
-                        {...register("cep")}
-                        placeholder={errors.cep ? `${errors.cep.message}` : 'CEP'}
-                     />
+  return (
+    <CheckoutMain>
+      <CheckoutContainer className="firstContainer">
+        <CheckoutTitle>Complete seu pedido</CheckoutTitle>
 
-                     <InputForm
-                        gridArea="rua"
-                        {...register("rua")}
-                        placeholder={errors.rua ? `${errors.rua.message}` : 'Rua'}
-                     />
+        <form id="formOrder" onSubmit={handleSubmit(handleOrderData)}>
+          <FormContainer className="formAddress">
+            <CheckoutContainer className="titleFormAndPaymentContainer">
+              <MapPinLine className="mapPinLine" size={22} />
+              <TextSpan>Endereço de Entrega</TextSpan>
+              <TextParagraph>
+                Informe o endereço onde deseja receber seu pedido
+              </TextParagraph>
+            </CheckoutContainer>
 
-                     <InputForm
-                        gridArea="numero"
-                        {...register("numero", { required: true })}
-                        placeholder={errors.numero ? `${errors.numero.message}` : 'Numero'}
-                     />
+            <div className="formInputsAddressContainer">
+              <InputForm
+                gridArea="cep"
+                {...register('cep')}
+                placeholder={errors.cep ? `${errors.cep.message}` : 'CEP'}
+              />
 
-                     <InputForm
-                        gridArea="complemento"
-                        className="inputFormComplemento"
-                        {...register("complemento")}
-                        placeholder="Complemento"
-                     />
+              <InputForm
+                gridArea="rua"
+                {...register('rua')}
+                placeholder={errors.rua ? `${errors.rua.message}` : 'Rua'}
+              />
 
-                     <InputForm
-                        gridArea="bairro"
-                        {...register("bairro", { required: true })}
-                        placeholder={errors.bairro ? `${errors.bairro.message}` : 'Bairro'}
-                     />
+              <InputForm
+                gridArea="numero"
+                {...register('numero', { required: true })}
+                placeholder={
+                  errors.numero ? `${errors.numero.message}` : 'Numero'
+                }
+              />
 
-                     <InputForm
-                        gridArea="cidade"
-                        {...register("cidade", { required: true })}
-                        placeholder={errors.cidade ? `${errors.cidade.message}` : 'Cidade'}
-                     />
+              <InputForm
+                gridArea="complemento"
+                className="inputFormComplemento"
+                {...register('complemento')}
+                placeholder="Complemento"
+              />
 
-                     <InputForm
-                        gridArea="uf"
-                        {...register("uf", { required: true })}
-                        placeholder={errors.uf ? `${errors.uf.message}` : 'UF'}
-                     />
-                  </div>
-               </FormContainer>
+              <InputForm
+                gridArea="bairro"
+                {...register('bairro', { required: true })}
+                placeholder={
+                  errors.bairro ? `${errors.bairro.message}` : 'Bairro'
+                }
+              />
 
-               <FormContainer className="formSelectPayment">
-                  <CheckoutContainer className="titleFormAndPaymentContainer">
-                     <CurrencyDollar className="currencyDollar" size={22} />
-                     <TextSpan>Pagamento</TextSpan>
-                     <TextParagraph>O pagamento é feito na entrega. Escolha a forma que deseja pagar</TextParagraph>
-                  </CheckoutContainer>
+              <InputForm
+                gridArea="cidade"
+                {...register('cidade', { required: true })}
+                placeholder={
+                  errors.cidade ? `${errors.cidade.message}` : 'Cidade'
+                }
+              />
 
-                  <div className="formInputsPaymentContainer">
-                     {
-                        paymentMethods && paymentMethods.map(paymentMethod => (
-                           <FormPayment
-                              active={paymentMethod.description === paymentMethodSelected}
-                              onClick={() => handleChangePaymentMethods(paymentMethod.description)}
-                              key={paymentMethod.id}
-                              icon={paymentMethod.icon}
-                              name={paymentMethod.description}
-                           ></FormPayment>
-                        ))
-                     }
-                  </div>
-               </FormContainer>
-            </form>
+              <InputForm
+                gridArea="uf"
+                {...register('uf', { required: true })}
+                placeholder={errors.uf ? `${errors.uf.message}` : 'UF'}
+              />
+            </div>
+          </FormContainer>
 
-         </CheckoutContainer>
+          <FormContainer className="formSelectPayment">
+            <CheckoutContainer className="titleFormAndPaymentContainer">
+              <CurrencyDollar className="currencyDollar" size={22} />
+              <TextSpan>Pagamento</TextSpan>
+              <TextParagraph>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </TextParagraph>
+            </CheckoutContainer>
 
-         <CheckoutContainer className="secondContainer">
-            <CheckoutTitle>Cafés selecionados</CheckoutTitle>
-            
-            <ResumeCoffeesContainer>
+            <div className="formInputsPaymentContainer">
+              {paymentMethods &&
+                paymentMethods.map((paymentMethod) => (
+                  <FormPayment
+                    active={paymentMethod.description === paymentMethodSelected}
+                    onClick={() =>
+                      handleChangePaymentMethods(paymentMethod.description)
+                    }
+                    key={paymentMethod.id}
+                    icon={paymentMethod.icon}
+                    name={paymentMethod.description}
+                  ></FormPayment>
+                ))}
+            </div>
+          </FormContainer>
+        </form>
+      </CheckoutContainer>
 
-            </ResumeCoffeesContainer>
-         </CheckoutContainer>
-      </CheckoutMain>
-   );
+      <CheckoutContainer className="secondContainer">
+        <CheckoutTitle>Cafés selecionados</CheckoutTitle>
+
+        <ResumeCheckoutContainer>
+          {items?.map((product) => (
+            <CoffeeCardRow key={product.id}>
+              <CoffeeCard>
+                <CoffeeImage src={product.image} />
+
+                <ResumeItemsContainer>
+                  <CoffeeTitle>{product.title}</CoffeeTitle>
+
+                  <AmountItensContainer>
+                    <Minus
+                      onClick={() =>
+                        product.amount > 1 &&
+                        handleUpdateCoffeeAmount('remove', product.id)
+                      }
+                      className="minusAndPlus"
+                      size={14}
+                      weight="bold"
+                    />
+                    <span>{product.amount}</span>
+                    <Plus
+                      onClick={() =>
+                        handleUpdateCoffeeAmount('add', product.id)
+                      }
+                      className="minusAndPlus"
+                      size={14}
+                      weight="bold"
+                    />
+                  </AmountItensContainer>
+
+                  <RemoveCoffeeButton
+                    onClick={() => handleItemDelete(product)}
+                    className="shoppingCart"
+                  >
+                    <Trash color={theme['purple-300']} size={16} />
+                    <span>REMOVER</span>
+                  </RemoveCoffeeButton>
+                </ResumeItemsContainer>
+
+                <CoffeePrice>
+                  {(product.total ? product.total : product.price)
+                    .toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })
+                    .toString()
+                    .replace('.', ',')}
+                </CoffeePrice>
+              </CoffeeCard>
+
+              <CoffeeRow />
+            </CoffeeCardRow>
+          ))}
+
+          <ResumeTotalCartContainer>
+            <TitleAndPriceComponent>
+              <TitleResumeCart>Total de itens</TitleResumeCart>
+              <PriceResumeCart>
+                {cartSubTotal
+                  ?.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                  .toString()
+                  .replace('.', ',')}
+              </PriceResumeCart>
+            </TitleAndPriceComponent>
+            <TitleAndPriceComponent>
+              <TitleResumeCart>Entrega</TitleResumeCart>
+              <PriceResumeCart>
+                {freight
+                  ?.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                  .toString()
+                  .replace('.', ',')}
+              </PriceResumeCart>
+            </TitleAndPriceComponent>
+            <TitleAndPriceComponent>
+              <TitleResumeCart className="lastChildTitle">
+                Total
+              </TitleResumeCart>
+              <PriceResumeCart className="lastChildTitle">
+                {cartTotal
+                  ?.toLocaleString('pt-br', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                  .toString()
+                  .replace('.', ',')}
+              </PriceResumeCart>
+            </TitleAndPriceComponent>
+          </ResumeTotalCartContainer>
+
+          <CheckedOrderButton form="formOrder" type="submit">
+            CONFIRMAR PEDIDO
+          </CheckedOrderButton>
+        </ResumeCheckoutContainer>
+      </CheckoutContainer>
+    </CheckoutMain>
+  )
 }
